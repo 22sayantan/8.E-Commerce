ed007aad326787e8d924cacc090dfa3d1fcc1261
sayantandolui97@gmail.com <sayantandolui97@gmail.com>
2023-07-24 19:53:31 +0530

    new entry

---
+++ src/components/pages/index/data/Data.jsx
@@ -0,0 +1,349 @@
+
+function Data() {
+    const Mobile_Data = 
+    [
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+        {
+            id: 1,
+            product_title: 'Iphone 13',
+            color: 'gray',
+            price: '10,000.00',
+            rating: '5⭐',
+            reviews: 43223
+        },
+    ]
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Data
